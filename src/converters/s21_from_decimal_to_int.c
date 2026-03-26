5f53b25af7e1843728bb62360ee9cdc878550758
gasem <al.mazharish.g@phystech.edu>
2026-03-27 02:30:04 +0300

    [fix] fixed logic in converters

--- src/converters/s21_from_decimal_to_int.c
+++ src/converters/s21_from_decimal_to_int.c
@@ -4,16 +4,24 @@
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
   if (!dst) return 1;
-  *dst = 0;
 
+  *dst = 0;
   s21_truncate(src, &src);
-  if (src.bits[1] || src.bits[2] || src.bits[0] >> 31) return 1;
-  if (s21_get_sign(src)) {
-    if (src.bits[0] > INT32_MAX + 1) return 1;
-    *dst -= src.bits[0];
+
+  if (src.bits[1] != 0 || src.bits[2] != 0) return 1;
+
+  uint32_t tmp = (uint32_t)src.bits[0];
+  int sign = s21_get_sign(src) != 0;
+
+  if (!sign) {
+    if (tmp > (uint32_t)INT_MAX) return 1;
+    *dst = (int)tmp;
   } else {
-    if (src.bits[0] > INT32_MAX) return 1;
-    *dst = src.bits[0];
+    if (tmp > ((uint32_t)INT_MAX + 1u)) return 1;
+    if (tmp == ((uint32_t)INT_MAX + 1u))
+      *dst = INT_MIN;
+    else
+      *dst = -(int)tmp;
   }
 
   return 0;
--- src/other/s21_floor.c
+++ src/other/s21_floor.c
@@ -1,6 +1,7 @@
 #include "../helpers/s21_helpers.h"
 #include "../s21_decimal.h"
 
+// Понял, что логика со сдвигами неверная, функция неправильная, надо переписать
 int s21_floor(s21_decimal value, s21_decimal *result) {
   int fl_finish = 0;
   if (!result)
@@ -11,16 +12,16 @@ int s21_floor(s21_decimal value, s21_decimal *result) {
     }
     int value_scale = s21_get_scale(value);
     if (!s21_get_sign(value)) {
-      result->bits[2] = result->bits[2] >> value_scale;
-      result->bits[2] = result->bits[2] << value_scale;
+      result->bits[0] = result->bits[0] >> value_scale;
+      result->bits[0] = result->bits[0] << value_scale;
     } else {
       if (value_scale) {
         big_decimal tmp;
         s21_expand(value, &tmp);
         s21_add_1(&tmp);
-        s21_normalize(tmp, &result);
-        result->bits[2] = result->bits[2] >> (value_scale - 1);
-        result->bits[2] = result->bits[2] << (value_scale - 1);
+        s21_normalize(tmp, result);
+        result->bits[0] = result->bits[0] >> (value_scale - 1);
+        result->bits[0] = result->bits[0] << (value_scale - 1);
       }
     }
     s21_truncate(*result, result);
