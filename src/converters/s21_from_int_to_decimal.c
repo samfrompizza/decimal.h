7ba46ab8795d5a5b4285efc20fbcbd721b7b3a32
gasem <al.mazharish.g@phystech.edu>
2026-03-27 01:01:08 +0300

    [add] s21_from_int_to_decimal.c implemented

---
+++ src/converters/s21_from_int_to_decimal.c
@@ -0,0 +1,18 @@
+#include "../helpers/s21_helpers.h"
+#include "../s21_decimal.h"
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst) {
+  if (!dst) return 1;
+  for (int i = 0; i < 4; i++) {
+    dst->bits[i] = 0;
+  }
+
+  if (src > 0) {
+    dst->bits[2] = src;
+  } else {
+    dst->bits[3] = MINUS;
+    dst->bits[2] = src ^ MINUS;
+  }
+
+  return 0;
+}
