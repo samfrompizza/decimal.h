99be6f62c2dafbc356aebba48d91cb3a52c44c33
hatchedr <hatchedr@student.21-school.ru>
2026-03-27 11:11:58 +0300

    [fix] fixed merge and s21_normalize

--- src/helpers/s21_helpers.c
+++ src/helpers/s21_helpers.c
@@ -7,6 +7,9 @@ int s21_cmp_abs_without_scale(big_decimal left, big_decimal right) {
             result = (left.bits[i] > right.bits[i]) ? 1 : -1;
             break;
         }
+    }
+    return result;
+}
 
 void s21_align_scale(big_decimal *left, big_decimal *right) {
   if (left->scale < right->scale) {
@@ -44,21 +47,19 @@ int s21_normalize(big_decimal src, s21_decimal *dst) {
     }
   }
   if (src.scale < 0) {
-    if (src.sign)
-      res = 2;
-    else
-      res = 1;
-  }
-
-  for (int i = 0; i < 4; i++) {
-    dst->bits[i] = 0;
-  }
-  dst->bits[0] = (uint32_t)src.bits[0];
-  dst->bits[1] = (uint32_t)src.bits[1];
-  dst->bits[2] = (uint32_t)src.bits[2];
-  s21_set_scale(dst, src.scale);
-  if (src.sign == 1) {
-    s21_set_sign(dst);
+    if (src.sign) res = 2;
+    else res = 1;
+  } else {
+    for (int i = 0; i < 4; i++) {
+        dst->bits[i] = 0;
+    }
+    dst->bits[0] = (uint32_t)src.bits[0];
+    dst->bits[1] = (uint32_t)src.bits[1];
+    dst->bits[2] = (uint32_t)src.bits[2];
+    s21_set_scale(dst, src.scale);
+    if (src.sign == 1) {
+        s21_set_sign(dst);
+    }
   }
   return res;
 }
