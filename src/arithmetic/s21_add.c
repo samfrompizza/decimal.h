04830b7247d4e887702779a9473842793cdbcf71
hatchedr <hatchedr@student.21-school.ru>
2026-03-28 17:11:22 +0300

    [add] added s21_add, s21_sub and support funcs

---
+++ src/arithmetic/s21_add.c
@@ -0,0 +1,30 @@
+#include "../s21_decimal.h"
+#include "../helpers/s21_helpers.h"
+
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+    for (int i = 0; i < 4; i++) result->bits[i] = 0;
+    int sign_1 = s21_get_sign(value_1);
+    int sign_2 = s21_get_sign(value_2);
+    big_decimal big_value_1, big_value_2, big_res;
+    for (int i = 0; i < 7; i++) big_res.bits[i] = 0;
+    s21_expand(value_1, &big_value_1);
+    s21_expand(value_2, &big_value_2);
+    s21_align_scale(&big_value_1, &big_value_2);
+    if ((sign_1 && sign_2) || (!sign_1 && !sign_2)) {
+        s21_add_mantissa(big_value_1, big_value_2, &big_res);
+        big_res.sign = big_value_1.sign;
+    } else {
+        int compare = s21_cmp_abs_without_scale(big_value_1, big_value_2);
+        if (compare == 1) {
+            s21_sub_mantissa(big_value_1, big_value_2, &big_res);
+            big_res.sign = big_value_1.sign;
+        } else if (compare == -1) {
+            s21_sub_mantissa(big_value_2, big_value_1, &big_res);
+            big_res.sign = big_value_2.sign;
+        } else {
+            big_res.sign = 0;
+        }
+    }
+    big_res.scale = big_value_1.scale;
+    return s21_normalize(big_res, result);
+}
--- src/helpers/s21_helpers.h
+++ src/helpers/s21_helpers.h
@@ -45,4 +45,6 @@ void s21_multiply_by_10(big_decimal *v);
 uint32_t s21_div_by_10(big_decimal *v);
 void s21_add_1(big_decimal *v);
 
+void s21_add_mantissa(big_decimal value_1, big_decimal value_2, big_decimal *result);
+void s21_sub_mantissa(big_decimal value_1, big_decimal value_2, big_decimal *result);
 #endif
