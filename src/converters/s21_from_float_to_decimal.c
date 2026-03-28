1b2d4967414d082b349ef5ea424da899aee3ccac
gasem <al.mazharish.g@phystech.edu>
2026-03-28 23:23:18 +0300

    Merget branchess to get s21_add and implemented float_to_decimal

--- src/converters/s21_from_float_to_decimal.c
+++ src/converters/s21_from_float_to_decimal.c
@@ -27,7 +27,24 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
 
   char *pt = strchr(sx, 'e');
   if (pt == NULL) pt = sx + strlen(sx);
+  int n = 0;
   for (; pt > sx; pt--) {
-    
+    if (*pt == '.') pt --;
+    s21_decimal tmp;
+    big_decimal big_tmp;
+    int num = *pt - '0';
+    s21_from_int_to_decimal(num, &tmp);
+    s21_expand(tmp, &big_tmp);
+    for (int i = 0; i < n; i++) s21_multiply_by_10(&big_tmp);
+    n++;
+    s21_normalize(big_tmp, &tmp);
+    s21_add(tmp, *dst, dst);
   }
+
+  if (*pt == 'e') {
+    int scale = (*(pt + 2) - '0') * 10 + *(pt + 3) - '0';
+    dst->bits[3] |= scale << 15;
+  }
+
+  return 0;
 }
