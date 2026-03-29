b41a70e9b52a8b384050f75df0c5fad4a952cb50
gasem <al.mazharish.g@phystech.edu>
2026-03-29 03:22:08 +0300

    [fix] fixed decimal_to_float int to uint

--- src/converters/s21_from_decimal_to_float.c
+++ src/converters/s21_from_decimal_to_float.c
@@ -7,11 +7,14 @@
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
   if (dst == NULL) return 1;
 
-  long double value = (long double)src.bits[0] +
-                      (long double)src.bits[1] * powl(2, 32) +
-                      (long double)src.bits[2] * powl(2, 64);
+  long double value = (long double)(uint32_t)src.bits[0] +
+                      (long double)(uint32_t)src.bits[1] * powl(2, 32) +
+                      (long double)(uint32_t)src.bits[2] * powl(2, 64);
+  if (isnan(value) || isinf(value)) return 1;
+
   value /= powl(10, s21_get_scale(src));
   if (s21_get_sign(src)) value = -value;
+
   *dst = (float)value;
   if (isnan(*dst) || isinf(*dst)) return 1;
 
